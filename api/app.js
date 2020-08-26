3c6d60a2403419d6485f1c60af09a18dbe22c78c
Fanny Petersen <petersen.fanny@gmail.com>
2020-08-26 12:03:34 +0200

    Add error message if invalid input (also covering misspellings),refactor componentDidMount

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const apiKey = "df83c801c177f8ca6787e8b393c5d9ac";
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -17,10 +17,14 @@ app.use((req, res, next) => {
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get('/search/:city',  (req, res) => {
+  
+  // validate req.params here
+
   const urlCity = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${apiKey}`;
   fetch(urlCity)
   .then(res => res.json())
-  .then(data=>res.send(data));
+  .then(data=>res.send(data))
+  .catch(err => 'ERROR HANDLING') // <--------- HÄÄR
 });
 
  app.post('/nearby', (req, res) => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,141 +1,166 @@
-import React, { Component } from 'react';
-import Search from './Search';
-import Temp from './Temp';
-import './App.css';
+import React, { Component } from "react";
+import Search from "./Search";
+import Temp from "./Temp";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiResponse: '',
-      value: '',
+      apiResponse: "",
+      value: "",
       cels: true,
     };
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({ value: event.target.value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
 
     if (!this.state.value.match(/^[a-zA-Z\s]*$/)) {
       this.setState({
         ...this.state,
-        value: '',
-        error: 'error',
+        value: "",
+        error: "error",
       });
-
     } else {
-      this.callAPI(this.state.value);
+      this.callAPICity(this.state.value);
 
       this.setState({
         ...this.state,
-        value: '',
-      }); 
+        value: "",
+      });
     }
   };
 
-  handleClick = event => {
+  handleClick = (event) => {
     const newState = { ...this.state };
     newState.cels = !newState.cels;
     this.setState({ ...newState });
   };
 
   dateToday() {
-    const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
-    const today = new Date().toLocaleDateString('en-US', dateOptions);
+    const dateOptions = { weekday: "long", month: "long", day: "numeric" };
+    const today = new Date().toLocaleDateString("en-US", dateOptions);
     return today;
   }
 
-  callAPI(city) {
-    fetch(`http://localhost:8080/search/${city}`)
-      .then(res => res.json())
-      .then(data =>
-        this.setState({
-          ...this.state,
-          apiResponse: {
-            weather: data.weather[0].description,
-            farenheit: data.main.temp,
-            celsius: Math.floor(data.main.temp - 273.15),
-            wind: data.wind.speed,
-            city: data.name,
-            icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-          },
-          error: '',
-        }),
-      )
-      .catch(err => err);
-  }
+  async callAPICity(city) {
+    const response = await fetch(`http://localhost:8080/search/${city}`);
+    const cityData = await response.json();
 
-  componentDidMount() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const long = position.coords.longitude;
-        const lat = position.coords.latitude;
-        const coordinates = { lat, long };
-        const location = {
-          method: 'POST',
-          headers: {
-            'content-Type': 'application/json',
-          },
-          body: JSON.stringify(coordinates),
-        };
+    if (!cityData.name) {
+      this.setState({
+        ...this.state,
+        value: "",
+        error: "error",
+      });
 
-        fetch('http://localhost:8080/nearby', location)
-          .then(res => res.json())
-          .then(data =>
-            this.setState({
-              ...this.state,
-              apiResponse: {
-                weather: data.current.weather[0].description,
-                farenheit: data.current.temp,
-                celsius: Math.floor(data.current.temp - 273.15),
-                wind: data.current.wind_speed,
-                city: data.timezone.replace(/^(.*[\\\/])/, '').replace(/_/g, ' '),
-                icon: `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`,
-              },
-            }),
-          )
-          .catch(err => err);
+    } else {
+      this.setState({
+        ...this.state,
+        apiResponse: {
+          weather: cityData.weather[0].description,
+          farenheit: cityData.main.temp,
+          celsius: Math.floor(cityData.main.temp - 273.15),
+          wind: cityData.wind.speed,
+          city: cityData.name,
+          icon: `http://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`,
+        },
+        error: "",
       });
     }
   }
 
+  async callAPINearby(location) {
+    const response = await fetch("http://localhost:8080/nearby", location);
+    console.log("RESPONSE", response);
+
+    const cityData = await response.json();
+
+    this.setState({
+      ...this.state,
+      apiResponse: {
+        weather: cityData.current.weather[0].description,
+        farenheit: cityData.current.temp,
+        celsius: Math.floor(cityData.current.temp - 273.15),
+        wind: cityData.current.wind_speed,
+        city: cityData.timezone.replace(/^(.*[\\/])/, "").replace(/_/g, " "),
+        icon: `http://openweathermap.org/img/wn/${cityData.current.weather[0].icon}@2x.png`,
+      },
+    });
+    // .catch(err => err);
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const long = position.coords.longitude;
+        const lat = position.coords.latitude;
+        const coordinates = { lat, long };
+        const location = {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+          },
+          body: JSON.stringify(coordinates),
+        };
+        return location;
+      });
+      /*   } else {
+      // some error handling
+      alert("NOPE"); */
+    }
+  }
+
+  async componentDidMount() {
+    const location = this.getLocation();
+    await this.callAPINearby(location);
+  }
+
   render() {
-    const showIfNoError = this.state.error ? 'no-show' : 'show';
-    const showIfError = this.state.error ? 'show' : 'no-show';
+    const showIfNoError = this.state.error ? "no-show" : "show";
+    const showIfError = this.state.error ? "show" : "no-show";
 
     return (
-      <div className='App'>
-
+      <div className="App">
         <header className={`App-header ${showIfNoError}`}>
-          <h1 className='App-title'>{this.state.apiResponse.city}</h1>
-          <h2 className='date'>{this.dateToday()}</h2>
+          <h1 className="App-title">{this.state.apiResponse.city}</h1>
+          <h2 className="date">{this.dateToday()}</h2>
         </header>
 
         <section className={`content ${showIfNoError}`}>
-          <p className='weather'>We have {this.state.apiResponse.weather},</p>
-        <Temp
-          cels={this.state.cels}
-          farenheit={this.state.apiResponse.farenheit}
-          celsius={this.state.apiResponse.celsius}
-          handleClick={this.handleClick}
-        />
-        <p className='wind'>and wind speed is {this.state.apiResponse.wind} m/s.</p> <br></br>
-        <img className='icon' src={this.state.apiResponse.icon} alt='pretty weather icon'></img>
+          <p className="weather">We have {this.state.apiResponse.weather},</p>
+          <Temp
+            cels={this.state.cels}
+            farenheit={this.state.apiResponse.farenheit}
+            celsius={this.state.apiResponse.celsius}
+            handleClick={this.handleClick}
+          />
+          <p className="wind">
+            and wind speed is {this.state.apiResponse.wind} m/s.
+          </p>{" "}
+          <br></br>
+          <img
+            className="icon"
+            src={this.state.apiResponse.icon}
+            alt="pretty weather icon"
+          ></img>
         </section>
         <section className={`content--error ${showIfError}`}>
           <p>Please input a valid city</p>
         </section>
 
-        <Search 
-          handleChange={this.handleChange} 
-          handleSubmit={this.handleSubmit} 
-          value={this.state.value} 
+        <Search
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          value={this.state.value}
         />
-        
       </div>
     );
   }
